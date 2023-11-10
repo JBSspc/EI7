6378cbb524304bedfe3d5608828c3b3632f3e984
Sofía Palacios <77463414+JBSspc@users.noreply.github.com>
2023-11-10 12:01:48 -0600

    Update VersionReactiva_JOS.R
    Se resolvió el problema de las etiquetas. Funcionalidad necesaria para hacer la entrega en Moodle

--- VersionReactiva_JOS.R
+++ VersionReactiva_JOS.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(dplyr)
 
 # Cargamos los datos ---------------------------------
-load("db_lupus_cv.RData")
+load("db_lupus2.RData")
 
 # Definir la UI --------------------------------------
 ui <- fluidPage(
@@ -16,7 +16,7 @@ ui <- fluidPage(
       selectInput(
         inputId = "variable", # Pregunta de calidad de vida
         label = "Aspecto relacionado con la calidad de vida: ",
-        choices = colnames(rdata[,43:68])
+        choices = colnames(db_lupus2[,2:27])
       ),
     ),
     # Salidas
@@ -29,7 +29,7 @@ ui <- fluidPage(
 # Definir el servidor -----------------------------------
 server <- function(input, output) {
   reactive_data <- reactive({
-    rdata %>%
+    db_lupus2 %>%
       count(dx_time, .data[[input$variable]], wt = NULL)
   })
   
@@ -42,11 +42,12 @@ server <- function(input, output) {
       xlab("Tiempo de diagnóstico (años)") +
       ylab("Incidencia") +
       ggtitle(paste("Tiempo de diagnóstico y", input$variable)) +
-      scale_colour_hue(labels = c("Mala", "Normal", "Buena", "Muy buena")) +
+      scale_colour_hue() +
       guides(colour = guide_legend(title = paste("¿Cómo puntuaría su", input$variable, "?"))) +
       theme(text = element_text(size = 15))
   })
 }
+#scale_colour_hue(labels = c("Mala", "Normal", "Buena", "Muy buena")) +
 
 # Crear un objeto de Shiny app --------------------
 shinyApp(ui=ui, server = server)
