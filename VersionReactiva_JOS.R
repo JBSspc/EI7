11fd89b206ab5143ecc44b6b136c24e36312920c
Sofía Palacios <77463414+JBSspc@users.noreply.github.com>
2023-11-17 18:52:13 -0600

    Update VersionReactiva_JOS.R
    Imprime el texto de la introducción que escribió Josh. Aún no se resuleve lo de la descripción de cada gráfico.

--- VersionReactiva_JOS.R
+++ VersionReactiva_JOS.R
@@ -10,6 +10,9 @@ load("descripcionGraf.RData")
 # Definir la UI --------------------------------------
 ui <- fluidPage(
   titlePanel("Calidad de vida de un paciente con Lupus"),
+  textOutput("Introtitle"),
+  textOutput("Introtext"),
+  
   
   sidebarLayout(
     # Entradas
@@ -24,6 +27,7 @@ ui <- fluidPage(
         label = "Descripción del gráfico ",
         choices = colnames(df)
       ),
+    
     ),
     # Salidas
     mainPanel(
@@ -39,6 +43,11 @@ server <- function(input, output) {
     db_lupus2 %>%
       count(dx_time, .data[[input$variable]], wt = NULL)
   })
+  
+  output$Introtitle <-renderText({"Introducción"})
+  output$Introtext<- renderText({"El presente proyecto se enfoca en el diseño e implementación de una aplicación que permite visualizar las variables de tiempo de diagnóstico y calidad de vida para los sujetos con diagnóstico de lupus. 
+En esta entrega, se incluyen gráficos estáticos que se  intregarán en un ambiente reactivo, con el fin de que el usuario pueda escoger una variable relacionada con la calidad de vida y pueda visualizar cómo ha sido la calidad de vida de los pacientes con lupus, según el tiempo de diagnóstico que tienen (desde el dianóstico formal hasta el momento en que se registró en la base de datos). Lo anterior, puede ayudarles a conocer algunas características que podrían esperar de la enfermedad en los próximos años."})
+  
   
   des <- reactive({
     data[, input$varaibleGraf, drop = FALSE]
